a6708895127e03bb6b21100bbc8d11a65602d9c2
miketorii <miketorii@gmail.com>
2025-01-15 12:05:15 +0000

    add french words

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 export default function Home() {
   return (
     <div>
-          <li>je</li>
-          <li>tu</li>
+    <table>
+    <thead>
+      <tr>
+         <th>mot</th>
+         <th>意味</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>velo</td>
+        <td>自転車</td>
+      </tr>
+      <tr>
+        <td>verre</td>
+        <td>グラス</td>
+      </tr>
+    </tbody>
+    </table>
     </div>
   );
 }
