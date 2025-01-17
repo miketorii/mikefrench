59b00b0eb0ae617c15de6097d4764d38314d1bca
miketorii <miketorii@gmail.com>
2025-01-17 12:00:06 +0000

    add mot

--- app/page.tsx
+++ app/page.tsx
@@ -4,36 +4,26 @@ export default function Home() {
     <table>
     <thead>
       <tr>
-         <th>mot</th>
-         <th>意味</th>
-         <th>mot</th>
-         <th>意味</th>	 
+         <th>mot</th><th>意味</th><th>mot</th><th>意味</th>
+	 <th>mot</th><th>意味</th><th>mot</th><th>意味</th>	 
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>velo</td>
-        <td>自転車</td>
-        <td>duex</td>
-        <td>2</td>	
+        <td>velo</td><td>自転車</td><td>duex</td><td>2</td>
+	<td>la serviette</td>ナプキン<td>miam</td><td>おいしそう</td>
       </tr>
       <tr>
-        <td>verre</td>
-        <td>グラス</td>
-        <td>trois</td>
-        <td>3</td>	
+        <td>verre</td><td>グラス</td><td>trois</td><td>3</td>
+        <td>aujourd'hui</td><td>今日</td><td>placard</td><td>戸棚</td>	
       </tr>
       <tr>
-        <td>gâteau</td>
-        <td>ケーキ</td>
-        <td>quatre</td>
-        <td>4</td>	
+        <td>gâteau</td><td>ケーキ</td><td>quatre</td><td>4</td>
+        <td>ouvert</td><td>開いている</td><td>tasse</td><td>カップ</td>	
       </tr>
       <tr>
-        <td>cadeau</td>
-        <td>プレゼント</td>
-        <td>cinq</td>
-        <td>5</td>	
+        <td>cadeau</td><td>プレゼント</td><td>cinq</td><td>5</td>
+        <td>maintenant</td><td>今</td><td>De rien</td><td>どういたしまして</td>	
       </tr>      
     </tbody>
     </table>
