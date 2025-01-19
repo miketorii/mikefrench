ac45c07a102688cc8b6ff26cbfd3e3feaca20a1e
miketorii <miketorii@gmail.com>
2025-01-19 02:30:40 +0000

    updated words page

--- app/words/page.tsx
+++ app/words/page.tsx
@@ -1,43 +1,38 @@
 import Link from 'next/link';
 
 export default function WordsPage() {
-  return (
-    <div>
-    <table>
-    <thead>
-      <tr>
-         <th>mot</th><th>意味</th><th>mot</th><th>意味</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>velo</td><td>自転車</td><td>duex</td><td>2</td>
-      </tr>
-      <tr>
-	<td>la serviette</td><td>ナプキン</td><td>miam</td><td>おいしそう</td>
-      </tr>      
-      <tr>
-        <td>verre</td><td>グラス</td><td>trois</td><td>3</td>
-      </tr>
-      <tr>
-        <td>aujourd&#39;hui</td><td>今日</td><td>placard</td><td>戸棚</td>	
-      </tr>      
-      <tr>
-        <td>gâteau</td><td>ケーキ</td><td>quatre</td><td>4</td>
-      </tr>
-      <tr>
-        <td>ouvert</td><td>開いている</td><td>tasse</td><td>カップ</td>	
-      </tr>
-      <tr>
-        <td>cadeau</td><td>プレゼント</td><td>cinq</td><td>5</td>
-      </tr>
-      <tr>
-        <td>maintenant</td><td>今</td><td>De rien</td><td>どういたしまして</td>	
-      </tr>      
-    </tbody>
-    </table>
-    <p />
-    <Link href="/"><button>back</button></Link>
-    </div>
-  );
+
+       const words = [
+       	     { key: '1', word: 'velo', translation: '自転車'},
+       	     { key: '2', word: 'duex', translation: '2'},
+       	     { key: '3', word: 'la serviette', translation: 'ナプキン'},	
+       	     { key: '4', word: 'miam', translation: 'おいしそう'},
+       	     { key: '5', word: 'verre', translation: 'グラス'},
+       	     { key: '6', word: 'trois', translation: '3'},
+       	     { key: '7', word: 'aujourd&#39;hui', translation: '今日'},
+       	     { key: '8', word: 'placard', translation: '戸棚'},
+       	     { key: '9', word: 'gâteau', translation: 'ケーキ'},
+       	     { key: '10', word: 'quatre', translation: '4'},
+       	     { key: '11', word: 'ouvert', translation: '開いている'},
+       	     { key: '12', word: 'tasse', translation: 'カップ'},
+       	     { key: '13', word: 'cadeau', translation: 'プレゼント'},
+       	     { key: '14', word: 'cinq', translation: '5'},
+       	     { key: '15', word: 'maintenant', translation: '今'},
+       	     { key: '16', word: 'De rien', translation: 'どういたしまして'}
+       ];
+
+       return (
+       <div>
+	<table>
+	<thead>
+		<tr><th>mot</th><th>意味</th></tr>
+	</thead>
+	<tbody>
+		{ words.map( (obj) => ( <tr key={obj.key} ><td>{obj.word}</td><td>{obj.translation}</td></tr> )  ) }	
+	</tbody>
+	</table>
+	<p />
+	<Link href="/"><button>back</button></Link>
+       </div>
+       );
 };
