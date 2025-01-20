5dcc8a6af9ac6498a9b86aec5b1bca4284a88002
miketorii <miketorii@gmail.com>
2025-01-20 13:33:43 +0000

    update sentences json

--- app/sentences/page.tsx
+++ app/sentences/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function SentencesPage(){
 
        const sentences = [
-       	     { key: '1', sentence: 'Ca coute cinq euros?', translated: 'これは５ユーロですか？' },
-       	     { key: '2', sentence: 'Tu veux quoi?', translated: '何が欲しいですか？' },
-       	     { key: '3', sentence: 'Pourquoi Leo est ici?', translated: 'レオは何故ここにいますか？' }
+       	     { "key": "1", "sentence": "Ca coute cinq euros?", "translated": "これは５ユーロですか？" },
+       	     { "key": "2", "sentence": "Tu veux quoi?", "translated": "何が欲しいですか？" },
+       	     { "key": "3", "sentence": "Pourquoi Leo est ici?", "translated": "レオは何故ここにいますか？" }
        ];
 
        return (
