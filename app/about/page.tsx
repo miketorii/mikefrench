7cf3ad0ae635c1b1d286b791e194bb073abf15a9
miketorii <miketorii@gmail.com>
2025-01-26 02:50:51 +0000

    update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,12 +21,32 @@ export default function AboutPage(){
        	     	   	    {id: 9, name: "Item 9"}   
 			    ];
 
+       const words = `
+       [
+       	     { "key": "1", "word": "velo", "translation": "自転車"},
+       	     { "key": "2", "word": "duex", "translation": "2"},
+       	     { "key": "3", "word": "la serviette", "translation": "ナプキン"},	
+       	     { "key": "4", "word": "miam", "translation": "おいしそう"},
+       	     { "key": "5", "word": "verre", "translation": "グラス"},
+       	     { "key": "6", "word": "trois", "translation": "3"},
+       	     { "key": "7", "word": "aujourd'hui", "translation": "今日"},
+       	     { "key": "8", "word": "placard", "translation": "戸棚"},
+       	     { "key": "9", "word": "gâteau", "translation": "ケーキ"},
+       	     { "key": "10", "word": "quatre", "translation": "4"},
+       	     { "key": "11", "word": "ouvert", "translation": "開いている"},
+       	     { "key": "12", "word": "tasse", "translation": "カップ"}	     
+	]
+	`;
+
+       ///////////////////////////////////////
+       //
        const ITEMS_PER_PAGE = 3;
        const totalPages = Math.ceil(data.length / ITEMS_PER_PAGE);
 
        const [currentPage, setCurrentPage] = useState(1);
 
        const handlePageChange = (page: number) => {
+	     
 	     if (page <= 0){
 	     	page = 1;
 	     }
@@ -42,7 +62,26 @@ export default function AboutPage(){
 	     const endIndex   = startIndex + ITEMS_PER_PAGE;
 	     return data.slice(startIndex, endIndex);
        };
-       
+
+       ///////////////////////////////////////
+       //
+       const wordstmp = JSON.parse(words);
+       const dispwords = [];
+
+       const createPaginatedWordData = () => {
+       	     const ITEMS_PER_PAGE2 = 4;
+       	     const startIndex = (currentPage-1) * ITEMS_PER_PAGE2;
+	     const endIndex   = startIndex + ITEMS_PER_PAGE2;
+	     
+	      for (let i = startIndex; i < endIndex; i+=2 ){
+	      	  if( i+1 < words.length ){
+		      dispwords.push( <tr key={wordstmp[i].key} ><td>{wordstmp[i].word}</td><td>{wordstmp[i].translation}</td><td>{wordstmp[i+1].word}</td><td>{wordstmp[i+1].translation}</td></tr> );
+		  }
+	      }
+
+	      return dispwords;
+      };
+
        return (
        <div>
               <h1>This is About page</h1>
@@ -51,6 +90,16 @@ export default function AboutPage(){
 	      <ul>
 		      { getPaginatedData().map( (item: Item) => ( <li key={item.id}>{item.name}</li> ) ) }
 	      </ul>	      
+	      <p />
+	      <table>
+	      <thead>
+		<tr><th>mot</th><th>意味</th><th>mot</th><th>意味</th></tr>
+	      </thead>
+	      <tbody>
+	      	 { createPaginatedWordData() }		
+	      </tbody>
+	      </table>	
+	      <p />
 	      <button key="but1" onClick={ () => handlePageChange( currentPage+1 ) } >next</button>
 	      <p />
 	      <button key="but2" onClick={ () => handlePageChange( currentPage-1 ) } >back</button>	      
