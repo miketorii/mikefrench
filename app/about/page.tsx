4d8518fb0d681dda86dfdc58ce41d67ff2efdeef
miketorii <miketorii@gmail.com>
2025-01-26 01:48:40 +0000

    modified about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,7 +15,10 @@ export default function AboutPage(){
        	     	   	    {id: 3, name: "Item 3"},
        	     	   	    {id: 4, name: "Item 4"},
        	     	   	    {id: 5, name: "Item 5"},
-       	     	   	    {id: 6, name: "Item 6"}			    
+       	     	   	    {id: 6, name: "Item 6"},
+       	     	   	    {id: 7, name: "Item 7"},
+       	     	   	    {id: 8, name: "Item 8"},
+       	     	   	    {id: 9, name: "Item 9"}   
 			    ];
 
        const ITEMS_PER_PAGE = 3;
@@ -23,7 +26,16 @@ export default function AboutPage(){
 
        const [currentPage, setCurrentPage] = useState(1);
 
-       const handlePageChange = (page: number) => { setCurrentPage(page); };
+       const handlePageChange = (page: number) => {
+	     if (page <= 0){
+	     	page = 1;
+	     }
+       	     if (page > totalPages){
+	     	page = totalPages;
+	     }
+
+	     setCurrentPage(page);
+       };
 
        const getPaginatedData = () => {
        	     const startIndex = (currentPage-1) * ITEMS_PER_PAGE;
@@ -39,9 +51,9 @@ export default function AboutPage(){
 	      <ul>
 		      { getPaginatedData().map( (item: Item) => ( <li key={item.id}>{item.name}</li> ) ) }
 	      </ul>	      
-	      <button key="but1" onClick={ () => handlePageChange( 2 ) } >next</button>
+	      <button key="but1" onClick={ () => handlePageChange( currentPage+1 ) } >next</button>
 	      <p />
-	      <button key="but2" onClick={ () => handlePageChange( 1 ) } >back</button>	      
+	      <button key="but2" onClick={ () => handlePageChange( currentPage-1 ) } >back</button>	      
 	      <Link href="/"><button className="mk-button mk-light-gray mk-right">back</button></Link>
        </div>
        );
