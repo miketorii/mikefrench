69d98601250a2f319ecf04518a870b09c1976058
miketorii <miketorii@gmail.com>
2025-01-27 13:37:29 +0000

    update word page

--- app/words/page.tsx
+++ app/words/page.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function WordsPage() {
 
@@ -21,28 +24,66 @@ export default function WordsPage() {
        	     { "key": "15", "word": "maintenant", "translation": "今"},
        	     { "key": "16", "word": "De rien", "translation": "どういたしまして"},
        	     { "key": "17", "word": "quand", "translation": "いつ"},
-       	     { "key": "18", "word": "près d'ici", "translation": "この近く"}	     
+       	     { "key": "18", "word": "près d'ici", "translation": "この近く"},
+       	     { "key": "19", "word": "la boulangerie", "translation": "パン屋"},
+       	     { "key": "20", "word": "la banque", "translation": "銀行"},
+       	     { "key": "21", "word": "l'appartement", "translation": "アパート"},
+       	     { "key": "22", "word": "le jardin", "translation": "庭"},
+       	     { "key": "23", "word": "la musique", "translation": "音楽"},
+       	     { "key": "24", "word": "chocolat", "translation": "チョコレート"}	     
 	]
 	`;       
 
        const wordstmp = JSON.parse(words);
-       const dispwords = [];
-       for (let i = 0; i < wordstmp.length; i+=2 ){
-       	   if( i+1 < words.length ){
-	     dispwords.push( <tr key={wordstmp[i].key} ><td>{wordstmp[i].word}</td><td>{wordstmp[i].translation}</td><td>{wordstmp[i+1].word}</td><td>{wordstmp[i+1].translation}</td></tr> );
-	   }
-	}
+       
+       const ITEMS_PER_PAGE = 8;
+       const totalPages = Math.ceil(wordstmp.length / ITEMS_PER_PAGE);
+
+       const [currentPage, setCurrentPage] = useState(1);
+
+       const handlePageChange = (page: number) => {
+	     
+	     if (page <= 0){
+	     	page = 1;
+	     }
+       	     if (page > totalPages){
+	     	page = totalPages;
+	     }
+
+	     setCurrentPage(page);
+       };
+
+       const createPaginatedWordData = () => {
+       	     const dispwords = [];
+       
+       	     const startIndex = (currentPage-1) * ITEMS_PER_PAGE;
+	     const endIndex   = startIndex + ITEMS_PER_PAGE;
+	     
+	      for (let i = startIndex; i < endIndex; i+=2 ){
+	      	  if( i+1 < words.length ){
+		      dispwords.push( <tr key={wordstmp[i].key} ><td>{wordstmp[i].word}</td><td>{wordstmp[i].translation}</td><td>{wordstmp[i+1].word}</td><td>{wordstmp[i+1].translation}</td></tr> );
+		  }
+	      }
+
+	      return dispwords;
+      };
 
        return (
        <div>
+	<h2>Page: {currentPage} / {totalPages}</h2>
 	<table>
 	<thead>
 		<tr><th>mot</th><th>意味</th><th>mot</th><th>意味</th></tr>		
 	</thead>
 	<tbody>
-		{ dispwords }
+	      	 { createPaginatedWordData() }		
 	</tbody>
-	</table>	
+	</table>
+	<p />
+	<button key="buttonnext" onClick={ () => handlePageChange( currentPage+1 ) } >next</button>
+	<p />
+	<button key="buttonback" onClick={ () => handlePageChange( currentPage-1 ) } >back</button>
+	<p />
 	<Link href="/"><button className="mk-button mk-light-gray mk-left">back</button></Link>
        </div>
        );
