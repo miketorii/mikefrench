c39227ea0b96f4c1800a3bf536984deb2838a61a
miketorii <miketorii@gmail.com>
2025-02-15 01:12:09 +0000

    add sentences

--- app/api/getsentences/route.tsx
+++ app/api/getsentences/route.tsx
@@ -11,12 +11,29 @@ export async function GET() {
        	     { "key": "5", "sentence": "Peut-etre, je ne sais pas.", "translated": "たぶん、わかりません" },
        	     { "key": "6", "sentence": "Nous sommes perdus.", "translated": "私たちは道に迷いました" },  
        	     { "key": "7", "sentence": "Pourquoi Leo est ici?", "translated": "レオは何故ここにいますか？" },
-       	     { "key": "8", "sentence": "C'est l'oncle d'Omar?", "translated": "オマールのおじさんですか？" }	     
+       	     { "key": "8", "sentence": "C'est l'oncle d'Omar?", "translated": "オマールのおじさんですか？" },
+
+       	     { "key": "9", "sentence": "Je suis toujours à l'heure.", "translated": "私はいつも時間通りです" },
+       	     { "key": "10", "sentence": "Pas du tout, Monsieur", "translated": "いいえ、全然です、旦那様" },
+       	     { "key": "11", "sentence": "Vous voulez une autre part de pizza?", "translated": "ピザをもう一切れいかがですか？" },
+       	     { "key": "12", "sentence": "Vous voulez un autre café", "translated": "コーヒーをもう一杯いかがですか？" },
+
+       	     { "key": "13", "sentence": "Vous payez par carte?", "translated": "カードで払いますか？" },
+       	     { "key": "14", "sentence": "Où est la réunion?", "translated": "会議はどこですか？" },
+       	     { "key": "15", "sentence": "Notre fils travaille au Japon.", "translated": "私たちの息子は日本で働いています" },
+       	     { "key": "16", "sentence": "L'entreprise est à Paris?", "translated": "その会社はパリにありますか？" },	     
+	     
+       	     { "key": "17", "sentence": "Combien ca coute?", "translated": "いくらですか？" },
+       	     { "key": "18", "sentence": "De rien", "translated": "どういたしまして" },
+       	     { "key": "19", "sentence": "Il y a des crepes", "translated": "クレープがあります" },
+       	     { "key": "20", "sentence": "On y va!", "translated": "行きましょう" }
        ];
 
        return Response.json( sentences );
 
 /*
+       	     { "key": "", "sentence": "", "translated": "" }
+	     
        const filePath = path.join(__dirname,'data.json');
 
        console.log(filePath);
